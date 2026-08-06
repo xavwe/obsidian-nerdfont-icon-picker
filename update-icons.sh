fa43a556ec19038de56f17f523f83e2cab1a4660
Xaver Wenhart <git@xavwe.dev>
2026-08-06 02:06:10 +0200

    chore: make overwriting the icons.json the last step

--- update-icons.sh
+++ update-icons.sh
@@ -3,4 +3,4 @@
 tmpdir=$(mktemp -d)
 git clone --quiet --depth 1 --single-branch --branch master --filter=blob:none "https://github.com/ryanoasis/nerd-fonts" "$tmpdir"
 (cd "$tmpdir/bin/scripts" && ./generate-css.sh)  > /dev/null 2>&1
-cat "$tmpdir/glyphnames.json" | jq 'del(.METADATA) | to_entries | map({title: .key, icon: .value.char})' 
+cat "$tmpdir/glyphnames.json" | jq 'del(.METADATA) | to_entries | map({title: .key, icon: .value.char})' > ./src/icons.json
